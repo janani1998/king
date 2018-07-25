d517b676b2e96d8a8b845ba9fb11d8e472b504bb
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-25 10:03:00 +0530

    Create range.java

---
+++ range.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=8;
+		if(n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9||n==10)
+			System.out.print("YES");
+		else
+			System.out.print("NO");
+	}
+}
