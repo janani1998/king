aad406ebbf15b723aa39cf5dbd039654a811d264
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-25 10:41:56 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=2;
+	 	if(n%2==0)
+		System.out.print("YES");
+		else
+		System.out.print("NO");
+	}
+}
