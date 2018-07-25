8223ca4b17357f6e4b36a0f50da57e851cad1136
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-25 09:37:25 +0530

    Create Strntimes.java

---
+++ Strntimes.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   String s="laptop";
+	   int k=3;
+	   for(int i=0;i<k;i++)
+	   {
+	   	 System.out.println(s);
+	   }
+	}
+}
