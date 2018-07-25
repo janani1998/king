d99bda448bf582b611e9ffae58464eee69a8c5de
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-25 09:50:55 +0530

    Create strcom.java

---
+++ strcom.java
@@ -0,0 +1,18 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1="hhh";
+		String s2="zzzz";
+		int l1=s1.length();
+		int l2=s2.length();
+		if(l1>l2||l1==l2)
+		{
+			System.out.print(s1);
+		}
+		else if(l1<l2)
+		{
+			System.out.print(s2);
+		}
+	}
+}
