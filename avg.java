b4ba130d47c99305edf778d973321131f8a30fe1
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-25 10:28:49 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3,sum=0,avg=0;
+		int a[]={3,2,2};
+		for(int i=0;i<n;i++)
+		{
+		   sum=sum+a[i];
+		    avg=(sum/n);
+		}
+		System.out.print(avg);
+	}
+}
